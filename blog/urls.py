322052c216aab1fac31c6c05a165a775576188fd
palmertt <tristanrp96@gmail.com>
2021-09-15 17:40:57 -0400

    Added post model, added first post data and user

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import HomeView
+from .views import home
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='index')
+    path('', home, name='index')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
-from django.views.generic import TemplateView
+from django.shortcuts import redirect, render
+from .models import Post
 
 # Create your views here.
-class HomeView(TemplateView):
-    template_name = "blog/index.html"
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/index.html', context)
