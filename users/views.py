0c45ca1a98cd34a6c88055c982828baa7601b649
palmertt <tristanrp96@gmail.com>
2021-09-14 20:10:24 -0400

    Added home side panel info, initial register page

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import RegisterForm
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('')
+    else:
+        form = RegisterForm()
+    return render(request, 'users/register.html', {'form': form})
